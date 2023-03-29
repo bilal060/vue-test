9833c59b0cf67e12946d7615f837d9eb8497c186
muhammadbilal <ahmad_4al@hotmail.com>
2023-03-29 13:27:15 +0500

    latest code

--- src/views/TablePage.vue
+++ src/views/TablePage.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="rounded tablepage bg-white px-3 py-4">
     <h2>Table Page</h2>
+
+    <!-- <h3 v-if="catcherror">{{ catcherror }}</h3>
+    <v-data-table hide-default-footer :headers="data.heading" :items="data.details" class="elevation-1">
+    </v-data-table> -->
+
     <v-data-table hide-default-footer :headers="tableheader" :items="tabledata" class="elevation-1">
     </v-data-table>
   </div>
 </template>
 
 <script>
+
+// import axios from 'axios'
 export default {
   data: () => ({
+    // data: [],
+    // catcherror: '',
     tableheader: [
       { text: 'DAU', value: 'DAU', },
       { text: 'Month', value: 'month', align: 'right' },
@@ -78,8 +87,21 @@ export default {
       },
 
     ],
-
   }),
+  // created() {
+  //   this.getPost()
+  // },
+  // methods: {
+  //   getPost() {
+  //     axios.get('https://9e34429a-eba3-4e56-85f2-685706a08897.mock.pstmn.io/report/dau?dashboardUserId=abc123&periodFrom=1669852800&periodTo=1674063742&aggregateBy%5B0%5D=month&aggregateBy%5B1%5D=country%E2%80%9D')
+  //       .then((response) => {
+  //         this.data = response.data;
+  //       })
+  //       .catch((catcherror) => {
+  //         this.error = catcherror
+  //       })
+  //   }
+  // },
 }
 </script>
 <style scoped>
